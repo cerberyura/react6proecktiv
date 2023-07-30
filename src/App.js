70f417a34b3290989b9e9eff4b82b36de99409d2
Yurii Cerber <yuriicerber@gmail.com>
2023-07-30 16:15:28 +0000

    1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <div>
         <h2>Лічильник:</h2>
-        <h1>0</h1>
+        <h1>0 </h1>
         <button className="minus">- Мінус</button>
         <button className="plus">Плюс +</button>
       </div>
