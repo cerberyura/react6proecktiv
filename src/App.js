8f6e1ff2623528ea084547f4e621a41bac00e6da
Yurii Cerber <yuriicerber@gmail.com>
2023-07-30 18:40:32 +0000

    quiz

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import React from "react";
 import './index.scss';
 
-function App() {
+const questions = [
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    variants: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
 
-  const [count, SetCount] = React.useState(0)
-  const onClickPlus = () => {
-    SetCount(count + 1)
-    console.log(count)
-  }
-  const onClickMinus = () => {
-    if (count > 0) {
-      SetCount(count - 1)
-    }
-    console.log(count)
-  }
+function Result() {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали 3 ответа из 10</h2>
+      <button>Попробовать снова</button>
+    </div>
+  );
+}
+
+function Game() {
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: '50%' }} className="progress__inner"></div>
+      </div>
+      <h1>Что такое useState?</h1>
+      <ul>
+        <li>Это функция для хранения данных компонента</li>
+        <li>Это глобальный стейт</li>
+        <li>Это когда на ты никому не нужен</li>
+      </ul>
+    </>
+  );
+}
+
+function App() {
   return (
     <div className="App">
-      <div>
-        <h2>Лічильник:</h2>
-        <h1>{count}</h1>
-        <button onClick={() => onClickMinus()} className="minus">- Мінус</button>
-        <button onClick={() => onClickPlus()} className="plus">Плюс +</button>
-      </div>
+      <Game />
+      {/* <Result /> */}
     </div>
   );
 }
